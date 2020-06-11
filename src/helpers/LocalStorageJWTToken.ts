17a5b0c7fee5f3ee33869e9dbc42754ab12c3850
Martin Desrumaux <martin.desrumaux@ircam.fr>
2020-06-11 15:07:19 +0200

    feat(JWTToken): Add InMemoryJWTToken

---
+++ src/helpers/LocalStorageJWTToken.ts
@@ -0,0 +1,68 @@
+import { JWTToken } from './JWTToken'
+import { PersistentJWTToken } from './PersistentJWTToken'
+
+export class LocalStorageJWTToken implements PersistentJWTToken {
+  protected _token: Readonly<JWTToken | undefined>;
+  readonly localStorageKey: string;
+
+  constructor (localStorageKey = 'timeside-api-token') {
+    this.localStorageKey = localStorageKey
+  }
+
+  init () {
+    // 'jwt-decode' can throw InvalidTokenError
+    try {
+      this._token = this.getFromPersistentStorage()
+      if (!this._token) {
+        return
+      }
+      if (this._token.refresh.isExpired()) {
+        this.removeToken()
+      }
+    } catch (e) {
+      console.error(`Invalid token (${JSON.stringify(e.message)}). Deleting it from localStorage`)
+      this.removeToken()
+    }
+  }
+
+  private getFromPersistentStorage (): JWTToken | undefined {
+    if (!window.localStorage) {
+      throw new Error('window.localStorage is undefined')
+    }
+    const raw = localStorage.getItem(this.localStorageKey)
+    if (!raw) {
+      return undefined
+    }
+    return JWTToken.fromSerialized(raw)
+  }
+
+  updateAccessToken (accessTokenBase64: string) {
+    if (!this._token) {
+      throw new Error('unable to set access token without refresh token')
+    }
+    this._token.updateAccessToken(accessTokenBase64)
+    this.saveToken()
+  }
+
+  hasToken () {
+    return this._token !== undefined
+  }
+
+  get token () {
+    return this._token
+  }
+
+  set token (token: JWTToken) {
+    this._token = token
+    this.saveToken()
+  }
+
+  saveToken () {
+    localStorage.setItem(this.localStorageKey, this._token.serialize())
+  }
+
+  removeToken () {
+    this._token = undefined
+    localStorage.removeItem(this.localStorageKey)
+  }
+}
